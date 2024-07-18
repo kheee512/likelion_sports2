a47f1ed6053e1ad587e3621f4da4cc89eb2a855e
dong-wook <boogiewooki02@gmail.com>
2024-07-18 16:39:41 +0900

    캘린더 기능 구현 진행중

--- teamtrack/src/App.js
+++ teamtrack/src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CalendarScreen from "./pages/CalendarScree";
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/calendar/:sport" element={<CalendarScreen />} />
       </Routes>
     </div>
   );
